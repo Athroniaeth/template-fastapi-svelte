a34c77529549bbb3b2360d5e0ae228f5625f0988
Athroniaeth <pierre.chaumont@hotmail.fr>
2025-02-06 23:52:32 +0100

    Finish prototype, update CLI, resign multipage

--- src/backend/_app.py
+++ src/backend/_app.py
@@ -1,53 +1,17 @@
-from contextlib import asynccontextmanager
-from typing import Optional
-
-import httpx
 from fastapi import FastAPI, Request
-from jinja2 import Template
-from starlette.responses import StreamingResponse
+from starlette.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
 
-# Create a global HTTP client to reuse connections
-client: Optional[httpx.AsyncClient] = None
+from backend import DIST_PATH
 
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    """Create and close the global HTTP client."""
-    global client
-    client = httpx.AsyncClient(http2=True, follow_redirects=True)
-    yield
-    await client.aclose()
+# Create the FastAPI app
+app = FastAPI()
+templates = Jinja2Templates(directory=DIST_PATH)
+app.mount("/assets", StaticFiles(directory=DIST_PATH / "assets"), name="assets")
 
 
-# Create the FastAPI app with the lifespan context manager
-app = FastAPI(lifespan=lifespan)
-
-# Address of your Vite development server (change if needed)
-VITE_DEV_SERVER = "http://localhost:5173/"
-
-
-@app.get("/{full_path:path}")
-async def proxy_request(request: Request, full_path: str):
-    # Prepare the headers excluding those that may cause issues
-    headers = dict(request.headers)
-
-    # Build destination URL preserving the path and query params
-    url = f"{VITE_DEV_SERVER}{full_path}"
-
-    # Fetch the response from Vite using the global client
-    response = await client.get(url, params=request.query_params, headers=headers, timeout=None)
-
-    # Inject the auto-reload script in the HTML if the URL targets an HTML file
-    if url.endswith((".html", ".htm", "/")):
-        content = await response.aread()
-        html_text = content.decode("utf-8")
-        template = Template(html_text)
-        render_html = template.render({"name": "Hello World!"})
-        return StreamingResponse(content=render_html, media_type="text/html")
-
-    # Return the response as a streaming response
-    return StreamingResponse(
-        response.aiter_bytes(),
-        status_code=response.status_code,
-        headers=dict(response.headers),
+@app.get("/")
+def index(request: Request):
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "name": "Hello World!"}
     )
--- src/backend/_cli.py
+++ src/backend/_cli.py
@@ -1,6 +1,35 @@
+import logging
+
 from typer import Typer
 
-cli = Typer()
+cli = Typer(
+    help="CLI for the FastAPI application.",
+    add_completion=False,
+    no_args_is_help=True,
+    pretty_exceptions_enable=False,
+)
+
+
+@cli.command()
+def dev(
+    source: str = "src.backend._dev:app",
+    host: str = "localhost",
+    port: int = 8000,
+):
+    """Run the FastAPI application with the Vite development server."""
+    import uvicorn
+    import requests
+
+    logging.info("Checking if Vite development server is running...")
+
+    try:
+        requests.get("http://localhost:5173/", timeout=0.1)
+    except requests.exceptions.ConnectionError:
+        raise ValueError(
+            "Vite development server is not running. Please use `npm run dev` to start it."
+        )
+
+    uvicorn.run(source, host=host, port=port)
 
 
 @cli.command()
@@ -11,4 +40,5 @@ def run(
 ):
     """Run the FastAPI application."""
     import uvicorn
+
     uvicorn.run(source, host=host, port=port)
