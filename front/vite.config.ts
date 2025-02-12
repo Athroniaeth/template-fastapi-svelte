d448486a9158ef72096303df52cc4852d0e1b1fc
Pierre Chaumont <p.chaumont@alienor.net>
2025-02-12 13:34:15 +0100

    Update, add watcher to jinja templates folder

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,19 +1,42 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import type {Plugin, ViteDevServer} from 'vite'
+import {defineConfig} from 'vite'
+import {svelte} from '@sveltejs/vite-plugin-svelte'
+
+import fs from 'fs'
+import path from 'path'
+
+function watchJinja2(): Plugin {
+    return {
+        name: 'watch-jinja2',
+        configureServer(server: ViteDevServer) {
+            const edgeDir = path.resolve(__dirname, '..', 'templates')
+            fs.watch(edgeDir, {recursive: true}, (_, filename) => {
+                // @ts-ignore
+                if (filename.endsWith('.jinja2')) {
+                    server.ws.send({type: 'full-reload'})
+                }
+            })
+        },
+    }
+}
 
 export default defineConfig({
-    plugins: [svelte()],
-
+    plugins: [
+        svelte(),
+        watchJinja2(),
+    ],
     build: {
         manifest: true,
         rollupOptions: {
             input: {
-                main: 'src/main.ts'
-            }
-        }
+                main: 'src/main.ts',
+            },
+        },
     },
 
     server: {
-        cors: { origin: 'http://localhost:8000' },
-    }
+        cors: {
+            origin: 'http://localhost:8000',
+        },
+    },
 })
