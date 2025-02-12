65f14029b264e0aa2f6270b18216395db8b27be8
Pierre Chaumont <p.chaumont@alienor.net>
2025-02-12 13:55:53 +0100

    Update, reproduce copy paste to dist for templates

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,13 +4,16 @@ import {svelte} from '@sveltejs/vite-plugin-svelte'
 
 import fs from 'fs'
 import path from 'path'
+import {viteStaticCopy} from 'vite-plugin-static-copy'
 
 function watchJinja2(): Plugin {
     return {
         name: 'watch-jinja2',
         configureServer(server: ViteDevServer) {
             const edgeDir = path.resolve(__dirname, '..', 'templates')
-            fs.watch(edgeDir, {recursive: true}, (_, filename) => {
+
+            // @ts-ignore
+            fs.watch(edgeDir, {recursive: true}, (_: any, filename: string) => {
                 // @ts-ignore
                 if (filename.endsWith('.jinja2')) {
                     server.ws.send({type: 'full-reload'})
@@ -24,19 +27,26 @@ export default defineConfig({
     plugins: [
         svelte(),
         watchJinja2(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: '../templates/*.jinja2', // relative to project root
+                    dest: 'templates' // will be copied to `dist/templates`
+                }
+            ]
+        })
     ],
+
     build: {
         manifest: true,
         rollupOptions: {
             input: {
-                main: 'src/main.ts',
-            },
-        },
+                main: 'src/main.ts'
+            }
+        }
     },
 
     server: {
-        cors: {
-            origin: 'http://localhost:8000',
-        },
-    },
+        cors: {origin: 'http://localhost:8000'},
+    }
 })
